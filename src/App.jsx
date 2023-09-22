91622ec6c703e53e68d573f920e2d4b82bc6d072
Fahad Zahid <fahadktk542@gmail.com>
2023-09-22 19:58:16 +0000

    portfolio items still to be edited

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Fahad Zahid",
+  title: "Data Scientist & Machine Learning Engineer",
+  email: "fahadktk542@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a profession data Scientist and ML engineer with a ruthless problem solving mindset. One of the core constant of my job, throughout the career, has been to help my clients with machine learning solutions by leveraging the power of data.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Data Analysis & Visualization",
+  "Data Science",
+  "Machine Learning",
+  "Deep Learning",
+  "Databases",
+  "Python Scripting",
+  "Presentation & Reporting",
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my DS & ML experience I continually look for new and better ways to make the data a useful resource.";
 
 const About = () => {
   return (
